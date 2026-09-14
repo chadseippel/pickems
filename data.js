0a18979be1312fc43f3af84f98f24c0272f2b684
Chad Seippel <chadseippel@gmail.com>
2026-09-14 14:57:19 -0400

    Gary test

--- data.js
+++ data.js
@@ -222,7 +222,7 @@ async function loadData(winners, losers, games) {
         players_row.appendChild(player_name);
 
         if (player_name == 'Gary') {
-            player_name.innerHTML = "<font class=\"player_title\">"+ player + "</font><img src=\"images/gary.svg\" alt=\"description\">";
+            player_name.innerHTML = "<font class=\"player_title\">"+ player + "</font><img src=\"images/gary.svg\" alt=\"description\" class=\"name_decor\">";
         }
         else {
             player_name.innerHTML = "<font class=\"player_title\">"+ player + "</font>";
